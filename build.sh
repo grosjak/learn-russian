2421abcebaf4a149982e32ac718983faec513e9f
Jack <jack@localhost>
2025-12-27 18:28:00 +0100

    Disable compression storage and clear static chache

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static files
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
 # Apply migrations
 python manage.py migrate
